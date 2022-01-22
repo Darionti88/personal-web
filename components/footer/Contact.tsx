14ed1040d1f043a14f7bb61ffaf099031c2ce219
Darionti88 <darionti@gmail.com>
2022-01-21 23:53:57 -0300

    Fix Footer Container

--- components/footer/Contact.tsx
+++ components/footer/Contact.tsx
@@ -4,10 +4,10 @@ import { Contact } from "../../interface/CardInterface";
 const MyContact = ({ contact, url, content }: Contact) => {
   return (
     <li>
-      <span className='text-2xl underline  decoration-4 decoration-sky-500 font-nunito'>
+      <span className=' text-xl sm:text-2xl underline  decoration-4 decoration-sky-500 font-nunito'>
         {contact}
       </span>
-      <a className='font-roboto text-2xl' href={url}>
+      <a className='font-roboto text-md sm:text-2xl' href={url}>
         {content}
       </a>
     </li>
--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -6,9 +6,10 @@ const Footer = () => {
   return (
     <footer
       id='contact'
-      className='h-full w-full bg-background400 sm:px-20 space-y-7 py-10 flex flex-col lg:justify-center lg:items-center z-60 '>
-      <div className='flex space-x-7'>
-        <h1 className='text-2xl self-start font-nunito underline decoration-4 decoration-primary-orange'>
+      className='h-full w-full bg-background400 sm:px-20 z-20 space-y-7 
+      py-10 flex flex-col lg:justify-center lg:items-center '>
+      <div className='flex flex-col space-y-8 space-x-7'>
+        <h1 className='text-2xl self-center sm:self-start font-nunito underline decoration-4 decoration-primary-orange'>
           Contact me at.
         </h1>
         <div>
