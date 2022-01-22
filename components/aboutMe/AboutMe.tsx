f55a1369543ec65ff10fa1facb52bf12c08175a6
Darionti88 <darionti@gmail.com>
2022-01-22 14:54:21 -0300

    Edit About Section

--- components/aboutMe/AboutMe.tsx
+++ components/aboutMe/AboutMe.tsx
@@ -12,14 +12,13 @@ const AboutMe = () => {
         <p
           className='xl:text-2xl text-1xl text-grey-400  py-7 
       font-roboto z-20'>
-          Hi! My name is Dario Ontiveros and I come from Buenos Aires,
-          Argentina. 👋 I am a FullStack Developer, mostly working in Mobile
-          with React Native, but I have worked with React.Js and Node.Js I have
-          7 years of experience working in the 🎥 Movies and Advertising
-          Industry. This taught me all about working in teams, how to lead and
-          how to be led. It gave me a sense of what it means to work in
-          projects, defining strategies, establishing deadlines and having a
-          common goal.
+          Hi! 👋 My name is Dario Ontiveros and I come from Buenos Aires,
+          Argentina. I am a FullStack Developer, mostly working in Mobile with
+          React Native, but I also work with React and Node.Js. 7 years working
+          in the 🎥 Movies and Advertising Industry taught me all about working
+          in teams, how to lead and how to be led. It gave me a sense of what it
+          means to work in projects, defining strategies, establishing deadlines
+          and having a common goals.
         </p>
       </div>
     </section>
